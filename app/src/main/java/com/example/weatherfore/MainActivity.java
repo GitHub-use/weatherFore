740c1a2f67a34e8b902526578a5fbd2d8af22249
gxh <1146816404@qq.com>
2019-12-04 23:26:34 +0800

    update

--- app/src/main/java/com/example/weatherfore/MainActivity.java
+++ app/src/main/java/com/example/weatherfore/MainActivity.java
@@ -4,9 +4,13 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.weatherfore.model.CityList;
@@ -22,6 +26,18 @@ public class MainActivity extends AppCompatActivity {
     List<CityList> cityListList = null;
 
     WeatherInfo weatherInfo = null;
+
+    final int FINSH_NET_LOADING = 92;
+
+
+    private Handler han = new Handler(){
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            if(msg.what==FINSH_NET_LOADING){
+                init();
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +63,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void init(){
-
+        TextView textView_city_info = findViewById(R.id.city_info);
+        textView_city_info.setText(weatherInfo.getHeWeather6().get(0).getBasic().getParent_city()+"--"+weatherInfo.getHeWeather6().get(0).getBasic().getLocation());
+        ImageView imageView_info_show = findViewById(R.id.info_show_image);
+        System.out.println(weatherInfo.getHeWeather6().get(0).getNow().getCond_txt());
+        imageView_info_show.setImageResource(getResources().getIdentifier("p"+weatherInfo.getHeWeather6().get(0).getNow().getCond_code(),"drawable",getBaseContext().getPackageName()));
+        TextView textView_info_ = findViewById(R.id.info_show_text);
+        textView_info_.setText(weatherInfo.getHeWeather6().get(0).getNow().getFl());
+        List<WeatherInfo.HeWeather6Bean.DailyForecastBean> tt = weatherInfo.getHeWeather6().get(0).getDaily_forecast();
     }
 
     @Override
@@ -60,7 +83,8 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if(item.getItemId()==R.id.menu_change_city){
             if(finsh_net_loading){
-                getSupportFragmentManager().beginTransaction().add(R.id.main_layout_fragment,new SelectCityFragment(SelectCityFragment.FIRST_STATUS,cityListList)).addToBackStack(null).commit();
+                //getSupportFragmentManager().beginTransaction().add(R.id.main_layout_fragment,new SelectCityFragment(SelectCityFragment.FIRST_STATUS,cityListList)).addToBackStack(null).commit();
+                getSupportFragmentManager().beginTransaction().addToBackStack(null).add(R.id.main_layout_fragment,new SelectCityFragment(SelectCityFragment.FIRST_STATUS)).commit();
                 System.out.println(cityListList.size());
                 Log.d("MainActivity:", "onOptionsItemSelected: loading fragment");
             }else{
@@ -68,5 +92,18 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         return true;
+    }
+
+    public void getWeatherInfo(final String localId){
+        new Thread(){
+            @Override
+            public void run() {
+                weatherInfo = GetWeatherInfo.getWeatherInfo(localId);
+                Message msg = new Message();
+                msg.what=FINSH_NET_LOADING;
+                han.sendMessage(msg);
+
+            }
+        }.start();
     }
 }
--- app/src/main/java/com/example/weatherfore/SelectCityFragment.java
+++ app/src/main/java/com/example/weatherfore/SelectCityFragment.java
@@ -1,9 +1,12 @@
 package com.example.weatherfore;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -15,6 +18,7 @@ import com.example.weatherfore.model.CityItemAdapter;
 import com.example.weatherfore.model.CityList;
 import com.example.weatherfore.model.CityNextList;
 import com.example.weatherfore.model.LocalId;
+import com.example.weatherfore.util.GetWeatherInfo;
 
 import java.util.List;
 
@@ -30,31 +34,95 @@ public class SelectCityFragment extends Fragment {
     List<LocalId> localIds=null;
 
     int tag;
+    int fomfrom;//来自哪里
+    int netfrom;
+
+
+    int FINISH_NET_LOADING = 17;
+
+    public Handler han = new Handler(){
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            if(msg.what==FINISH_NET_LOADING){
+                init();
+            }
+        }
+    };
+
     public SelectCityFragment(int t, List<?> objects){
         switch (t){
-            case FIRST_STATUS:cityLists = (List<CityList>) objects;break;
+            case FIRST_STATUS:
+                new Thread(){
+                    @Override
+                    public void run() {
+                        cityLists = GetWeatherInfo.getCityList();
+                        if(cityLists!=null){
+                            Message msg = new Message();
+                            msg.what=FINISH_NET_LOADING;
+                            han.sendMessage(msg);
+                        }
+                    }
+                }.start();
+                ;
+//                cityLists = (List<CityList>) objects;
+                break;
             case SECOND_STATUS:cityNextLists = (List<CityNextList>) objects;break;
             case THIRD_STATUS:localIds = (List<LocalId>) objects;break;
         }
         tag=t;
     }
+    public SelectCityFragment(int t){
+        tag=t;
+        new Thread(){
+            @Override
+            public void run() {
+                cityLists = GetWeatherInfo.getCityList();
+                if(cityLists!=null){
+                    Message msg = new Message();
+                    msg.what=FINISH_NET_LOADING;
+                    han.sendMessage(msg);
+                }
+            }
+        }.start();
+    }
+
+    public SelectCityFragment(int t,final int j){
+        tag=t;
+        fomfrom=j;
+        new Thread(){
+            @Override
+            public void run() {
+                cityNextLists = GetWeatherInfo.getCityNext(j);
+                if(cityNextLists!=null){
+                    Message msg = new Message();
+                    msg.what=FINISH_NET_LOADING;
+                    han.sendMessage(msg);
+                }
+            }
+        }.start();
+    }
+
+    public SelectCityFragment(int t,final int j,final int i){
+        tag=t;
+        fomfrom=j;
+        netfrom=i;
+        new Thread(){
+            @Override
+            public void run() {
+                localIds=GetWeatherInfo.getLocalId(j,i);
+                System.out.println(localIds.size());
+                if(localIds!=null){
+                    Message msg = new Message();
+                    msg.what=FINISH_NET_LOADING;
+                    han.sendMessage(msg);
+                }
+            }
+        }.start();
+    }
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        ListView listView = getActivity().findViewById(R.id.listview_selectcity);
-        ArrayAdapter arrayAdapter =null;
-        if(tag==FIRST_STATUS){
-            System.out.println("创建适配器");
-            arrayAdapter = new CityItemAdapter(getContext(),R.layout.city_item,cityLists,CityItemAdapter.FIRST_STATUS);
-            System.out.println(cityLists.size());
-        }
-        if(tag==SECOND_STATUS){
-
-        }
-        if(tag==THIRD_STATUS){
-
-        }
-        listView.setAdapter(arrayAdapter);
     }
 
     @Nullable
@@ -63,5 +131,51 @@ public class SelectCityFragment extends Fragment {
         View view = inflater.inflate(R.layout.selectcityfragment,container,false);
         mView = view;
         return view;
+    }
+
+    private void init(){
+        ListView listView = getActivity().findViewById(R.id.listview_selectcity);
+        ArrayAdapter arrayAdapter =null;
+        if(tag==FIRST_STATUS){
+            System.out.println("创建适配器");
+            arrayAdapter = new CityItemAdapter(getContext(),R.layout.city_item,cityLists,CityItemAdapter.FIRST_STATUS);
+            System.out.println(cityLists.size());
+        }
+        if(tag==SECOND_STATUS){
+            arrayAdapter = new CityItemAdapter(getContext(),R.layout.city_item,cityNextLists,CityItemAdapter.SECOND_STATUS);
+        }
+        if(tag==THIRD_STATUS){
+            System.out.println(localIds.size());
+            arrayAdapter = new CityItemAdapter(getContext(),R.layout.city_item,localIds,CityItemAdapter.THIRD_STATUS);
+        }
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (tag){
+                    case FIRST_STATUS:
+                        System.out.println("-----------------");
+                        CityList temp = (CityList) parent.getItemAtPosition(position);
+                        getActivity().getSupportFragmentManager().beginTransaction().addToBackStack(null).replace(R.id.main_layout_fragment,new SelectCityFragment(SelectCityFragment.SECOND_STATUS,temp.getId())).commit();
+                        break;
+                    case SECOND_STATUS:
+                        System.out.println("this");
+                        CityNextList temp1 = (CityNextList) parent.getItemAtPosition(position);
+                        getActivity().getSupportFragmentManager().beginTransaction().addToBackStack(null).replace(R.id.main_layout_fragment,new SelectCityFragment(SelectCityFragment.THIRD_STATUS,fomfrom,temp1.getId())).commit();
+                        break;
+                    case THIRD_STATUS:
+                        LocalId dd = (LocalId) parent.getItemAtPosition(position);
+                        MainActivity mainActivity = (MainActivity) getActivity();
+                        mainActivity.getWeatherInfo(dd.getWeather_id());
+                        getActivity().getSupportFragmentManager().beginTransaction().remove(SelectCityFragment.this).commit();
+                        break;
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
+        super.onViewStateRestored(savedInstanceState);
     }
 }
